49b2954fa351230b61c3db428552b43987f0bbcf
Gitnix <gitnix1337@gmail.com>
2018-09-11 19:40:41 -0400

    added check for unsupported browser, small css tweak

--- client/index.js
+++ client/index.js
@@ -2,4 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Client from './client'
 
-ReactDOM.render(<Client />, document.getElementById('container'))
+if (
+	window.navigator.userAgent.includes('Chrome') ||
+	window.navigator.userAgent.includes('Firefox')
+) {
+	ReactDOM.render(<Client />, document.getElementById('container'))
+} else {
+	ReactDOM.render(
+		<div className="unsupported-browser">
+			{'Sorry. Currently only Chrome and Firefox are supported. :('}
+			<img style={{ gridArea: 'canvas' }} src="images/banner_snake.png" />
+		</div>,
+		document.getElementById('container'),
+	)
+}
