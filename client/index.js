67d6e6be3be7bd0839072656b33f013e8b311bd5
Gitnix <gitnix1337@gmail.com>
2018-09-12 12:14:38 -0400

    adjusted screen shown for non-chrome browsers

--- client/index.js
+++ client/index.js
@@ -2,17 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Client from './client'
 
-if (
-	window.navigator.userAgent.includes('Chrome') ||
-	window.navigator.userAgent.includes('Firefox')
-) {
+if (window.navigator.userAgent.includes('Chrome')) {
 	ReactDOM.render(<Client />, document.getElementById('container'))
 } else {
 	ReactDOM.render(
-		<div className="unsupported-browser">
-			{'Sorry. Currently only Chrome and Firefox are supported. :('}
-			<img style={{ gridArea: 'canvas' }} src="images/banner_snake.png" />
+		<div>
+			<div style={{ marginBottom: '1em' }}>
+				{
+					'Unfortunately, due to performance limitations, currently only the Chrome browser is supported. :('
+				}
+			</div>
+			<div>
+				<img src="images/banner_snake.png" />
+			</div>
 		</div>,
-		document.getElementById('container'),
+		document.getElementById('unsupported-browser'),
 	)
 }
